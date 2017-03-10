a4c794adb95c918014218c3ef503ae6fa46dee3e
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2017-03-10 17:50:35 -0300

    Fix constant

--- app/constants.js
+++ app/constants.js
@@ -6,21 +6,20 @@ import {
 
 const D = new Date();
 const HOURS = D.getHours();
-const MINS = D.getMinutes();
 const DAY = D.getDay();
 
-export const IS_FRIDAY_AFTERNOON = DAY === 5 && HOURS >= 16 && (HOURS < 17 || HOURS === 18 && MINS <= 30);
 export const IS_FRIDAY = DAY === 5;
+export const IS_FRIDAY_AFTERNOON = IS_FRIDAY && HOURS >= 17;
 
 export const getRandom = function(list) {
   return list[Math.floor((Math.random() * list.length))];
 };
 
 export   function dayHelper(){
-  if (IS_FRIDAY) {
-    return REASONS_TO_NOT_DEPLOY;
-  } else if (IS_FRIDAY_AFTERNOON) {
+  if (IS_FRIDAY_AFTERNOON) {
     return REASONS_FOR_FRIDAY_AFTERNOON;
+  } else if (IS_FRIDAY) {
+    return REASONS_TO_NOT_DEPLOY;
   }
   return REASONS_TO_DEPLOY;
 }
