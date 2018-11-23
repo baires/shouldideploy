3583976edf739df8ee062f7603ada7b79d10fe51
Mikk Tendermann <mikk150@gmail.com>
2018-11-23 14:06:36 +0200

    Sorry, english is not my mother tongue

--- app/reasons.js
+++ app/reasons.js
@@ -60,5 +60,5 @@ export const REASONS_FOR_WEEKEND = [
   'Beer?',
   'Drunk development is not a good idea!',
   'I see you deployed on Friday',
-  'Told you that Monday would be better idea!',
+  'Told you that Monday would be a better idea!',
 ]
