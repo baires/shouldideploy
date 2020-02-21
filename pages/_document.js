859894c96420e5030d62ea0bbdbce6c5a69c47b3
Alexis <alexis@sgarbossa.com.ar>
2020-02-21 16:55:17 -0300

    Fetch api to use use className for body

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
+import fetch from "isomorphic-unfetch";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import HeadElements from "../component/head";
 import { IS_FRIDAY } from "../helpers/constans";
-import BodyColor from "../component/body_color";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const res = await fetch("https://shouldideploy.today/api");
+    const json = await res.json();
+    console.log(json.shouldideploy);
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return { ...initialProps, shouldideploy: json.shouldideploy };
   }
 
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" type="image/png" href={IS_FRIDAY ? "/dots-red.png": "/dots.png"} sizes="32x32" />
+          <link rel="icon" type="image/png" href={IS_FRIDAY ? "/dots-red.png" : "/dots.png"} sizes="32x32" />
           <meta name="og:image" content={IS_FRIDAY ? "no.png" : "yes.png"} />
           <HeadElements />
         </Head>
-        <BodyColor bodyClass={IS_FRIDAY ? "its-friday" : null}>
+        <body className={!this.props.shouldideploy ? "its-friday" : null}>
           <Main />
           <NextScript />
-        </BodyColor>
+        </body>
       </Html>
     );
   }
