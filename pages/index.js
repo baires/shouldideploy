b54d33c6d8e78aade31525f8582de7a669f3e5f0
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2020-05-02 16:15:26 -0300

    Fix layout for Timezon component

--- pages/index.js
+++ pages/index.js
@@ -3,46 +3,45 @@ import Head from "next/head";
 import Time from "../helpers/time";
 import Widget from "../component/widget";
 import Footer from "../component/footer";
-import Timezone from '../component/timezone';
-import Router from 'next/router'
+import Timezone from "../component/timezone";
+import Router from "next/router";
 
 class Page extends React.Component {
-
   constructor(props) {
     super(props);
 
     let timezoneError = false;
 
     if (!Time.zoneExists(this.props.timezone)) {
-        timezoneError = true;
+      timezoneError = true;
     }
     this.state = {
-        timezone: timezoneError ? 'UTC' : this.props.timezone,
-        now: new Time(timezoneError ? 'UTC' : this.props.timezone),
+      timezone: timezoneError ? "UTC" : this.props.timezone,
+      now: new Time(timezoneError ? "UTC" : this.props.timezone),
     };
   }
 
   static async getInitialProps(request) {
-    let timezone = request.query.tz || 'UTC';
+    let timezone = request.query.tz || "UTC";
 
     return {
-        timezone: timezone
+      timezone: timezone,
     };
   }
 
   changeTimeZone = (timezone) => {
     if (!Time.zoneExists(this.props.timezone)) {
-        return ;
+      return;
     }
     let newUrl = new URL(location);
-        newUrl.searchParams.set('tz', timezone);
+    newUrl.searchParams.set("tz", timezone);
 
     Router.push(newUrl.pathname + newUrl.search);
     this.setState({
-        timezone: timezone,
-        now: new Time(timezone),
+      timezone: timezone,
+      now: new Time(timezone),
     });
-  }
+  };
 
   render() {
     return (
@@ -52,14 +51,18 @@ class Page extends React.Component {
             rel="icon"
             type="image/png"
             href={
-              this.state.now.isFriday() ? "https://shouldideploy.today/dots-red.png" : "https://shouldideploy.today/dots.png"
+              this.state.now.isFriday()
+                ? "https://shouldideploy.today/dots-red.png"
+                : "https://shouldideploy.today/dots.png"
             }
             sizes="32x32"
           />
           <meta
             property="og:image"
             content={
-                this.state.now.isFriday() ? "https://shouldideploy.today/no.png" : "https://shouldideploy.today/yes.png"
+              this.state.now.isFriday()
+                ? "https://shouldideploy.today/no.png"
+                : "https://shouldideploy.today/yes.png"
             }
           />
           <title>Should I Deploy Today?</title>
@@ -68,8 +71,16 @@ class Page extends React.Component {
           <div className="aligner">
             <Widget now={this.state.now} />
             <div className="meta">
-              <Footer timezone={this.state.timezone}/>
-              <Timezone onChange={this.changeTimeZone} timezone={this.state.timezone}/>
+              <Footer timezone={this.state.timezone} />
+              <ul className="footer-list">
+                <li>
+                  Timezone:{" "}
+                  <Timezone
+                    onChange={this.changeTimeZone}
+                    timezone={this.state.timezone}
+                  />
+                </li>
+              </ul>
             </div>
           </div>
         </div>
