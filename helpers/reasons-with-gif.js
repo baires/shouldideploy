31fc5267228d110c2f4206c77acfcbf0767d4f3a
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2021-07-15 19:24:10 +0530

    added some gifs along with reasons

--- helpers/reasons-with-gif.js
+++ helpers/reasons-with-gif.js
@@ -1,103 +1,414 @@
 export const REASONS_TO_DEPLOY = [
-  { reason: "I don't see why not", gifs: [] },
-  { reason: "It's a free country", gifs: [] },
-  { reason: 'Go ahead my friend!', gifs: [] },
-  { reason: 'Go for it', gifs: [] },
-  { reason: 'Go go go go!', gifs: [] },
-  { reason: "Let's do it!", gifs: [] },
-  { reason: 'Ship it! 🚢', gifs: [] },
-  { reason: 'Go with the flow 🌊', gifs: [] },
-  { reason: 'Harder better faster stronger', gifs: [] },
-  { reason: 'Rock on!', gifs: [] },
-  { reason: 'Make me proud', gifs: [] },
-  { reason: 'Break a leg!', gifs: [] },
-  { reason: 'This Is the Way', gifs: [] },
-  { reason: 'Strike First, Strike Hard, No Mercy!', gifs: [] }
+  {
+    reason: "I don't see why not",
+    gifs: [
+      'https://media.tenor.com/images/23f81f2b40ac6c3eadf77b3915d7b181/tenor.gif',
+      'https://media.giphy.com/media/l2SpKR1aR4FMBFQwE/giphy.gif',
+      'https://media.tenor.com/images/58249497103c35e8698aabb0b5707d5b/tenor.gif'
+    ]
+  },
+  {
+    reason: "It's a free country",
+    gifs: [
+      'https://media.tenor.com/images/05877d998c45d848c9a32ea932b76b63/tenor.gif',
+      'https://media.tenor.com/images/797b88574b1492fef3d555fefcd36480/tenor.gif',
+      'https://media.giphy.com/media/l3fZDDE6Z6R6sFCuI/giphy-downsized.gif'
+    ]
+  },
+  {
+    reason: 'Go ahead my friend!',
+    gifs: [
+      'https://media.tenor.com/images/57391227c9fadd2bb7bdfe039bcec9a3/tenor.gif',
+      'https://media.tenor.com/images/302142c9de443f9147e76ddedbe45be5/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Go for it',
+    gifs: [
+      'https://media.tenor.com/images/a0f7615911d4f88cede427ccaaf2e407/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Go go go go!',
+    gifs: [
+      'https://media1.tenor.com/images/de6b4658ea00d110600d3ff59c240f33/tenor.gif?itemid=11945751',
+      'https://media.giphy.com/media/7kn27lnYSAE9O/giphy.gif'
+    ]
+  },
+  {
+    reason: "Let's do it!",
+    gifs: [
+      'https://media.tenor.com/images/cc96098d77dfc363f2f93fb741c5f86c/tenor.gif',
+      'https://media.tenor.com/images/f224b5583500dedb62dd4b738c0d6475/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Ship it! 🚢',
+    gifs: [
+      'https://media.tenor.com/images/1ffa63f5dd4fe8fd45a039787071867a/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Go with the flow 🌊',
+    gifs: [
+      'https://media.tenor.com/images/e9d8b84813830b12f53708728bcd4861/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Harder better faster stronger',
+    gifs: [
+      'https://media.tenor.com/images/04de37446d429eb2f3edc85fe4101ced/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Rock on!',
+    gifs: ['https://media.giphy.com/media/B9O4oVqGjOBFu/giphy.gif']
+  },
+  {
+    reason: 'Make me proud',
+    gifs: [
+      'https://media.tenor.com/images/f1e1842bc23b6c87b0662be78fd31898/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Break a leg!',
+    gifs: ['https://media.giphy.com/media/aHs1EAnUAxYgU/giphy.gif']
+  },
+  {
+    reason: 'This Is the Way',
+    gifs: [
+      'https://media.tenor.com/images/53c67e922ab6c7d833c5cb8a2bc7ec76/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Strike First, Strike Hard, No Mercy!',
+    gifs: [
+      'https://media.tenor.com/images/04de37446d429eb2f3edc85fe4101ced/tenor.gif'
+    ]
+  }
 ]
 
 export const REASONS_TO_NOT_DEPLOY = [
-  { reason: "I wouldn't recommend it", gifs: [] },
-  { reason: "No, it's Friday", gifs: [] },
-  { reason: 'What about Monday?', gifs: [] },
-  { reason: 'Not today', gifs: [] },
-  { reason: 'Nope', gifs: [] },
-  { reason: 'Why?', gifs: [] },
-  { reason: 'Did the tests pass? Probably not', gifs: [] },
-  { reason: '¯\\_(ツ)_/¯', gifs: [] },
-  { reason: '😹', gifs: [] },
-  { reason: 'No', gifs: [] },
-  { reason: 'No. Breathe and count to 10, start again', gifs: [] },
-  { reason: "I'd rather have ice-cream 🍦", gifs: [] },
-  { reason: 'How could you? 🥺', gifs: [] }
+  {
+    reason: "I wouldn't recommend it",
+    gifs: [
+      'https://media.tenor.com/images/7ae3ecc2a00bfdc552c4b51a8fc44b0d/tenor.gif'
+    ]
+  },
+  {
+    reason: "No, it's Friday",
+    gifs: [
+      'https://media.tenor.com/images/56dfe4841d70be0ca39e60defb2dd36a/tenor.gif'
+    ]
+  },
+  {
+    reason: 'What about Monday?',
+    gifs: [
+      'https://media1.tenor.com/images/ce441e4f0e6f115e9eb1b321955c1b80/tenor.gif?itemid=5094560'
+    ]
+  },
+  {
+    reason: 'Not today',
+    gifs: [
+      'https://media.tenor.com/images/5c808c1380076b6eace30b0a4d6b2413/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Nope',
+    gifs: [
+      'https://media.tenor.com/images/934b14271e359e2bb18ed927d18f78ed/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Why?',
+    gifs: [
+      'https://media.tenor.com/images/44fd4d51724a476965341265dd09147a/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Did the tests pass? Probably not',
+    gifs: [
+      'hhttps://media1.tenor.com/images/f169f385f05a88996d5359867f0bc5ef/tenor.gif?itemid=4276898ttps://tenor.com/r6Mo.gif'
+    ]
+  },
+  {
+    reason: '¯\\_(ツ)_/¯',
+    gifs: [
+      'https://media.tenor.com/images/19f157eb345595245172e792ac12a30a/tenor.gif'
+    ]
+  },
+  {
+    reason: '😹',
+    gifs: [
+      'https://media1.tenor.com/images/675daa5c96e23bb7e2b5b06ef804cb81/tenor.gif?itemid=15881815'
+    ]
+  },
+  {
+    reason: 'No',
+    gifs: [
+      'https://media.tenor.com/images/934b14271e359e2bb18ed927d18f78ed/tenor.gif'
+    ]
+  },
+  {
+    reason: 'No. Breathe and count to 10, start again',
+    gifs: [
+      'https://media1.tenor.com/images/84ed599e3b630067902bef1cb9b8923f/tenor.gif?itemid=4981204'
+    ]
+  },
+  {
+    reason: "I'd rather have ice-cream 🍦",
+    gifs: [
+      'https://media.tenor.com/images/3621e1aa3041f2d0cf77cfa6e9658e76/tenor.gif'
+    ]
+  },
+  {
+    reason: 'How could you? 🥺',
+    gifs: ['https://media.giphy.com/media/JOLnvT9kh9QhMtjWMX/giphy.gif']
+  }
 ]
 
 export const REASONS_FOR_THURSDAY_AFTERNOON = [
-  { reason: 'You still want to sleep?', gifs: [] },
-  { reason: 'Call your partner!', gifs: [] },
-  { reason: 'Gonna stay late today?', gifs: [] },
-  { reason: 'Tell your boss that you found a bug and go home', gifs: [] },
-  { reason: 'What about Monday?', gifs: [] },
-  { reason: "I wouldn't recommend it", gifs: [] },
-  { reason: 'Not today', gifs: [] },
-  { reason: 'Nope', gifs: [] },
-  { reason: 'No. Breathe and count to 10, start again', gifs: [] }
+  {
+    reason: 'You still want to sleep?',
+    gifs: [
+      'https://media1.tenor.com/images/4cc3f1092383e28aac655acfcaac4a30/tenor.gif?itemid=4356005'
+    ]
+  },
+  {
+    reason: 'Call your partner!',
+    gifs: ['https://media.giphy.com/media/xT8qBu5gOYEqHhgDQs/giphy.gif']
+  },
+  {
+    reason: 'Gonna stay late today?',
+    gifs: ['https://media.giphy.com/media/emxKI1ROfMaKpMysA9/giphy.gif']
+  },
+  {
+    reason: 'Tell your boss that you found a bug and go home',
+    gifs: [
+      'https://media1.tenor.com/images/b068dd6bb5e51dec0d6c29ebecb40487/tenor.gif?itemid=4390513'
+    ]
+  },
+  {
+    reason: 'What about Monday?',
+    gifs: [
+      'https://media1.tenor.com/images/ce441e4f0e6f115e9eb1b321955c1b80/tenor.gif?itemid=5094560'
+    ]
+  },
+  {
+    reason: "I wouldn't recommend it",
+    gifs: [
+      'https://media.tenor.com/images/7ae3ecc2a00bfdc552c4b51a8fc44b0d/tenor.gif'
+    ]
+  },
+  {
+    reason: 'Not today',
+    gifs: [
+      'https://media1.tenor.com/images/d387b5b18f709a8cb00af727e8193273/tenor.gif?itemid=16392985'
+    ]
+  },
+  {
+    reason: 'Nope',
+    gifs: [
+      'https://media.tenor.com/images/934b14271e359e2bb18ed927d18f78ed/tenor.gif'
+    ]
+  },
+  {
+    reason: 'No. Breathe and count to 10, start again',
+    gifs: [
+      'https://media1.tenor.com/images/84ed599e3b630067902bef1cb9b8923f/tenor.gif?itemid=4981204'
+    ]
+  }
 ]
 
 export const REASONS_FOR_FRIDAY_AFTERNOON = [
-  { reason: 'Not by any chance', gifs: [] },
-  { reason: 'U mad?', gifs: [] },
-  { reason: 'What you are thinking?', gifs: [] },
-  { reason: 'No no no no no no no no', gifs: [] },
-  { reason: 'How do you feel about working nights and weekends?', gifs: [] },
-  { reason: '🔥 🚒 🚨 ⛔️ 🔥 🚒 🚨 ⛔️ 🔥 🚒 🚨 ⛔️', gifs: [] },
-  { reason: 'No! God! Please! No', gifs: [] },
-  { reason: 'No no no no no no no!', gifs: [] },
-  { reason: 'Keep dreaming darling', gifs: [] },
-  { reason: 'Why why Bro why?', gifs: [] },
-  { reason: 'But but but... why?', gifs: [] },
+  {
+    reason: 'Not by any chance',
+    gifs: [
+      'https://media1.tenor.com/images/adb9c5362d7df68690ad39014189b334/tenor.gif?itemid=6222488'
+    ]
+  },
+  {
+    reason: 'U mad?',
+    gifs: [
+      'https://media1.tenor.com/images/47598a309beb33584ad2fb4e29ba2a2a/tenor.gif?itemid=14006831'
+    ]
+  },
+  {
+    reason: 'What you are thinking?',
+    gifs: ['https://media.giphy.com/media/Ar16NNXfPm2gR4o40W/giphy.gif']
+  },
+  {
+    reason: 'No no no no no no no no',
+    gifs: ['https://media.giphy.com/media/23BST5FQOc8k8/giphy.gif']
+  },
+  {
+    reason: 'How do you feel about working nights and weekends?',
+    gifs: [
+      'https://media1.tenor.com/images/e3eca1a07a1922838b372d610a637c5d/tenor.gif?itemid=17924926'
+    ]
+  },
+  {
+    reason: '🔥 🚒 🚨 ⛔️ 🔥 🚒 🚨 ⛔️ 🔥 🚒 🚨 ⛔️',
+    gifs: [
+      'https://media1.tenor.com/images/4a16b44bcbcc59f5f076ce7a72d87310/tenor.gif?itemid=6098038'
+    ]
+  },
+  {
+    reason: 'No! God! Please! No',
+    gifs: [
+      'https://media1.tenor.com/images/071c91a60ebb6ab9f150f3edd8f30c30/tenor.gif?itemid=5606969'
+    ]
+  },
+  {
+    reason: 'No no no no no no no!',
+    gifs: ['https://media.giphy.com/media/23BST5FQOc8k8/giphy.gif']
+  },
+  {
+    reason: 'Keep dreaming darling',
+    gifs: ['https://media.giphy.com/media/QajnsRHmCKqjWjr37z/giphy.gif']
+  },
+  {
+    reason: 'Why why Bro why?',
+    gifs: [
+      'https://media1.tenor.com/images/81d3933eac32b4996934aadd23a6f7a1/tenor.gif?itemid=14239510'
+    ]
+  },
+  {
+    reason: 'But but but... why?',
+    gifs: [
+      'https://media1.tenor.com/images/81d3933eac32b4996934aadd23a6f7a1/tenor.gif?itemid=14239510'
+    ]
+  },
   {
     reason: 'Deploys are for Monday, so you can fix them till Friday.',
-    gifs: []
+    gifs: ['https://media.giphy.com/media/vguRpQzGag7M5h4UVt/giphy.gif']
   },
-  { reason: 'YOLO ! You only live once !', gifs: [] }
+  {
+    reason: 'YOLO ! You only live once !',
+    gifs: ['https://media.giphy.com/media/XbbOqRJF4G7Fbn5B20/giphy.gif']
+  }
 ]
 
 export const REASONS_FOR_FRIDAY_13TH = [
-  { reason: "Man, really? It's friday the 13th!", gifs: [] },
-  { reason: 'Do you believe in bad luck?', gifs: [] },
-  { reason: 'Jason is watching you', gifs: [] },
+  {
+    reason: "Man, really? It's friday the 13th!",
+    gifs: ['https://media.giphy.com/media/oOTTyHRHj0HYY/giphy.gif']
+  },
+  {
+    reason: 'Do you believe in bad luck?',
+    gifs: ['https://media.giphy.com/media/t2bWfTQQy7UGPOH38Q/giphy.gif']
+  },
+  {
+    reason: 'Jason is watching you',
+    gifs: ['https://media.giphy.com/media/hSiHQDHsy2z3EnnTtE/giphy.gif']
+  },
   {
     reason: 'If you want to spend your weekend in Crystal Lake, go ahead',
-    gifs: []
+    gifs: ['https://media.giphy.com/media/5cBBUJGiIniZZWtBSa/giphy.gif']
   },
-  { reason: 'To pray is no help if you take this bad decision', gifs: [] },
-  { reason: 'Did you look at the calendar today?', gifs: [] },
-  { reason: '📅 Friday the 13th. What do you think about it?', gifs: [] },
-  { reason: 'Just no!', gifs: [] },
-  { reason: 'But but but... why?', gifs: [] }
+  {
+    reason: 'To pray is no help if you take this bad decision',
+    gifs: ['https://media.giphy.com/media/LpX6j0dPySImVAZ4kz/giphy.gif']
+  },
+  {
+    reason: 'Did you look at the calendar today?',
+    gifs: ['https://media.giphy.com/media/sruPMYBvY56CX3G5gx/giphy.gif']
+  },
+  {
+    reason: '📅 Friday the 13th. What do you think about it?',
+    gifs: ['https://media.giphy.com/media/oOTTyHRHj0HYY/giphy.gif']
+  },
+  {
+    reason: 'Just no!',
+    gifs: [
+      'https://media1.tenor.com/images/adb9c5362d7df68690ad39014189b334/tenor.gif?itemid=6222488'
+    ]
+  },
+  {
+    reason: 'But but but... why?',
+    gifs: ['https://media.giphy.com/media/l2JhtKtDWYNKdRpoA/giphy.gif']
+  }
 ]
 
 export const REASONS_FOR_AFTERNOON = [
-  { reason: 'You still want to sleep?', gifs: [] },
-  { reason: 'Call your partner!', gifs: [] },
-  { reason: 'Gonna stay late today?', gifs: [] },
-  { reason: 'Tomorrow?', gifs: [] },
-  { reason: 'No', gifs: [] },
-  { reason: 'Tell your boss that you found a bug and go home', gifs: [] },
-  { reason: 'You have full day ahead of you tomorrow!', gifs: [] },
+  {
+    reason: 'You still want to sleep?',
+    gifs: [
+      'https://media1.tenor.com/images/4cc3f1092383e28aac655acfcaac4a30/tenor.gif?itemid=4356005'
+    ]
+  },
+  {
+    reason: 'Call your partner!',
+    gifs: ['https://media.giphy.com/media/xT8qBu5gOYEqHhgDQs/giphy.gif']
+  },
+  {
+    reason: 'Gonna stay late today?',
+    gifs: ['https://media.giphy.com/media/emxKI1ROfMaKpMysA9/giphy.gif']
+  },
+  {
+    reason: 'Tell your boss that you found a bug and go home',
+    gifs: [
+      'https://media1.tenor.com/images/b068dd6bb5e51dec0d6c29ebecb40487/tenor.gif?itemid=4390513'
+    ]
+  },
+  {
+    reason: 'Tomorrow?',
+    gifs: ['https://media.giphy.com/media/c7XiPNBKA0suc/giphy.gif']
+  },
+  {
+    reason: 'No',
+    gifs: ['https://media.giphy.com/media/23BST5FQOc8k8/giphy.gif']
+  },
+  {
+    reason: 'You have full day ahead of you tomorrow!',
+    gifs: ['https://media.giphy.com/media/hUCLh0jZNSQKv1HFMb/giphy.gif']
+  },
   {
     reason:
       "Trust me, (s)he will be much happier if it wasn't broken for a night",
-    gifs: []
+    gifs: ['https://media.giphy.com/media/gHOAu2veIeySCvVzMs/giphy.gif']
   },
-  { reason: 'How much do you trust your logging tools?', gifs: [] }
+  {
+    reason: 'How much do you trust your logging tools?',
+    gifs: ['https://media.giphy.com/media/9RyvxZv8NJBozq1NV1/giphy.gif']
+  }
 ]
 
 export const REASONS_FOR_WEEKEND = [
-  { reason: "Go home, you're drunk", gifs: [] },
-  { reason: 'How about Monday?', gifs: [] },
-  { reason: 'Beer?', gifs: [] },
-  { reason: 'Drunk development is not a good idea!', gifs: [] },
-  { reason: 'I see you deployed on Friday', gifs: [] },
-  { reason: 'Told you that Monday would be a better idea!', gifs: [] }
+  {
+    reason: "Go home, you're drunk",
+    gifs: [
+      'https://media1.tenor.com/images/b068dd6bb5e51dec0d6c29ebecb40487/tenor.gif?itemid=4390513'
+    ]
+  },
+  {
+    reason: 'How about Monday?',
+    gifs: [
+      'https://media1.tenor.com/images/ce441e4f0e6f115e9eb1b321955c1b80/tenor.gif?itemid=5094560'
+    ]
+  },
+  {
+    reason: 'Beer?',
+    gifs: [
+      'https://media1.tenor.com/images/502190379126869ee7803de70cd87c1c/tenor.gif?itemid=22050096'
+    ]
+  },
+  {
+    reason: 'Drunk development is not a good idea!',
+    gifs: [
+      'https://media1.tenor.com/images/8b5b67e6e9ed392d18e83cd20dec6950/tenor.gif?itemid=17048582'
+    ]
+  },
+  {
+    reason: 'I see you deployed on Friday',
+    gifs: [
+      'https://media1.tenor.com/images/8e29aa5cbfe1ef379a489b032422efc1/tenor.gif?itemid=21612634'
+    ]
+  },
+  {
+    reason: 'Told you that Monday would be a better idea!',
+    gifs: [
+      'https://media1.tenor.com/images/d2ad2a482813fd0897516ea2078caadd/tenor.gif?itemid=15202602'
+    ]
+  }
 ]
