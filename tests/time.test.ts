64909b395d6b476b8a6e7997e8a5ff7d2c02ea50
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2023-03-29 12:19:13 -0300

    Add intial test

---
+++ tests/time.test.ts
@@ -0,0 +1,26 @@
+import Time from '../helpers/time'
+
+describe('Time Class', () => {
+  test('should correctly identify timezone existence', () => {
+    expect(Time.zoneExists('America/Argentina/Buenos_Aires')).toBe(true)
+    expect(Time.zoneExists('Invalid/Timezone')).toBe(false)
+  })
+
+  test('should return correct day based on custom date and timezone', () => {
+    const time1 = new Time('UTC', '2023-03-31')
+    const time2 = new Time('America/Argentina/Buenos_Aires', '2023-03-31')
+
+    expect(time1.isFriday()).toBe(true)
+    expect(time2.isFriday()).toBe(true)
+  })
+
+  test('should return shouldideploy status based on custom date and timezone', () => {
+    const time1 = new Time('UTC', '2023-03-31')
+    const time2 = new Time('America/Argentina/Buenos_Aires', '2023-03-31')
+
+    expect(time1.isFriday()).toBe(true)
+    expect(time2.isFriday()).toBe(true)
+  })
+
+  // Add more tests as needed for other Time class methods.
+})
