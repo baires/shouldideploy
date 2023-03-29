f3cd18cba7b09e5d6df3ce21745871cebd9800b0
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2023-03-29 15:35:03 -0300

    Add GH actions for test

--- tests/days.test.ts
+++ tests/days.test.ts
@@ -1,128 +1,100 @@
 import Time from '../helpers/time'
 
 describe('Time class', () => {
-  let time: Time
+  // Helper function to mock the Date class
+  function mockDate(dateString: string) {
+    const date = new Date(dateString)
+    jest.spyOn(global, 'Date').mockImplementation(() => date)
+  }
 
-  beforeEach(() => {
-    // Set the timezone to UTC for all tests
-    time = new Time('UTC')
+  afterEach(() => {
+    jest.restoreAllMocks()
   })
 
-  describe('isThursday()', () => {
-    it('returns true if today is Thursday', () => {
-      const date = new Date('March 30, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isThursday()).toBe(true)
-    })
-
-    it('returns false if today is not Thursday', () => {
-      const date = new Date('March 29, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isThursday()).toBe(false)
-    })
+  it('isThursday', () => {
+    mockDate('2023-03-30T00:00:00') // Thursday, March 30th, 2023
+    const time = new Time('UTC')
+    expect(time.isThursday()).toBe(true)
   })
 
-  describe('isFriday()', () => {
-    it('returns true if today is Friday', () => {
-      const date = new Date('March 31, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isFriday()).toBe(true)
-    })
-
-    it('returns false if today is not Friday', () => {
-      const date = new Date('March 30, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isFriday()).toBe(false)
-    })
+  it('isFriday', () => {
+    mockDate('2023-03-31T00:00:00') // Friday, March 31st, 2023
+    const time = new Time('UTC')
+    expect(time.isFriday()).toBe(true)
+  })
+  it('is13th', () => {
+    mockDate('2023-03-13T00:00:00') // March 13th, 2023
+    const time = new Time('UTC')
+    expect(time.is13th()).toBe(true)
   })
 
-  describe('is13th()', () => {
-    it('returns true if today is the 13th of the month', () => {
-      const date = new Date('April 13, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.is13th()).toBe(true)
-    })
-
-    it('returns false if today is not the 13th of the month', () => {
-      const date = new Date('April 14, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.is13th()).toBe(false)
-    })
+  it('isAfternoon', () => {
+    mockDate('2023-03-30T17:00:00') // March 30th, 2023, 17:00 (5 PM)
+    const time = new Time('UTC')
+    expect(time.isAfternoon()).toBe(true)
   })
 
-  describe('isAfternoon()', () => {
-    it('returns true if it is afternoon', () => {
-      const date = new Date('April 1, 2023 16:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isAfternoon()).toBe(true)
-    })
-
-    it('returns false if it is not afternoon', () => {
-      const date = new Date('April 1, 2023 10:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isAfternoon()).toBe(false)
-    })
+  it('isThursdayAfternoon', () => {
+    mockDate('2023-03-30T17:00:00') // Thursday, March 30th, 2023, 17:00 (5 PM)
+    const time = new Time('UTC')
+    expect(time.isThursdayAfternoon()).toBe(true)
   })
 
-  describe('isThursdayAfternoon()', () => {
-    it('returns true if today is Thursday afternoon', () => {
-      const date = new Date('March 30, 2023 16:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isThursdayAfternoon()).toBe(true)
-    })
-
-    it('returns false if today is not Thursday afternoon', () => {
-      const date = new Date('March 30, 2023 10:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isThursdayAfternoon()).toBe(false)
-    })
+  it('isFridayAfternoon', () => {
+    mockDate('2023-03-31T17:00:00') // Friday, March 31st, 2023, 17:00 (5 PM)
+    const time = new Time('UTC')
+    expect(time.isFridayAfternoon()).toBe(true)
   })
 
-  describe('isChristmas()', () => {
-    it('returns true if it is Christmas Day', () => {
-      const date = new Date('December 25, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isChristmas()).toBe(true)
-    })
-
-    it('returns false if it is not Christmas Day', () => {
-      const date = new Date('December 26, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isChristmas()).toBe(false)
-    })
+  it('isFriday13th', () => {
+    mockDate('2023-10-13T00:00:00') // Friday, October 13th, 2023
+    const time = new Time('UTC')
+    expect(time.isFriday13th()).toBe(true)
   })
 
-  describe('isNewYear()', () => {
-    it("returns true if it is New Year's Day", () => {
-      const date = new Date('January 1, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isNewYear()).toBe(true)
-    })
-
-    it("returns false if it is not New Year's Day", () => {
-      const date = new Date('January 2, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isNewYear()).toBe(false)
-    })
+  it('isWeekend', () => {
+    mockDate('2023-04-01T00:00:00') // Saturday, April 1st, 2023
+    const time = new Time('UTC')
+    expect(time.isWeekend()).toBe(true)
   })
 
-  describe('isHolidays()', () => {
-    it('returns true if it is Christmas Day', () => {
-      const date = new Date('December 25, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isHolidays()).toBe(true)
-    })
+  it('isDayBeforeChristmas', () => {
+    mockDate('2023-12-24T17:00:00') // December 24th, 2023, 17:00 (5 PM)
+    const time = new Time('UTC')
+    expect(time.isDayBeforeChristmas()).toBe(true)
+  })
 
-    it("returns true if it is New Year's Day", () => {
-      const date = new Date('January 1, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isHolidays()).toBe(true)
-    })
+  it('isChristmas', () => {
+    mockDate('2023-12-25T00:00:00') // December 25th, 2023
+    const time = new Time('UTC')
+    expect(time.isChristmas()).toBe(true)
+  })
 
-    it('returns false if it is not a holiday', () => {
-      const date = new Date('January 2, 2023 00:00:00')
-      jest.spyOn(time, 'now').mockImplementation(() => date)
-      expect(time.isHolidays()).toBe(false)
-    })
+  it('isNewYear', () => {
+    mockDate('2023-12-31T17:00:00') // December 31st, 2023, 17:00 (5 PM)
+    const time = new Time('UTC')
+    expect(time.isNewYear()).toBe(true)
+
+    mockDate('2024-01-01T00:00:00') // January 1st, 2024
+    const time2 = new Time('UTC')
+    expect(time2.isNewYear()).toBe(true)
+  })
+
+  it('isHolidays', () => {
+    mockDate('2023-12-24T17:00:00') // December 24th, 2023, 17:00 (5 PM)
+    const time = new Time('UTC')
+    expect(time.isHolidays()).toBe(true)
+
+    mockDate('2023-12-25T00:00:00') // December 25th, 2023
+    const time2 = new Time('UTC')
+    expect(time2.isHolidays()).toBe(true)
+
+    mockDate('2023-12-31T17:00:00') // December 31st, 2023, 17:00 (5 PM)
+    const time3 = new Time('UTC')
+    expect(time3.isHolidays()).toBe(true)
+
+    mockDate('2024-01-01T00:00:00') // January 1st, 2024
+    const time4 = new Time('UTC')
+    expect(time4.isHolidays()).toBe(true)
   })
 })
