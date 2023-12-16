ad14d1efb22fdb859bb59383045ee373e07dba3a
Alexis Sgarbossa <alexis@sgarbossa.com.ar>
2023-12-16 13:47:46 -0300

    Update coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,8 @@
 const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageDirectory: 'coverage',
 };
 
 export default config
