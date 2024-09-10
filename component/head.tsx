5c3cdad806acb869d44f5789eb7ce978697a2a53
Will Leinweber <will@users.noreply.github.com>
2024-09-10 05:56:01 -0500

    Fix incorrect apostrophe usage (#1240)

--- component/head.tsx
+++ component/head.tsx
@@ -7,26 +7,26 @@ const Head = () => {
       <meta name="viewport" content="width=device-width" />
       <meta
         name="description"
-        content="A friendly reminder to all Developers, Designers, CEO's"
+        content="A friendly reminder to all Developers, Designers, CEOs"
       />
 
       <meta itemProp="name" content="Should I Deploy Today" />
       <meta
         itemProp="description"
-        content="A friendly reminder to all Developers, Designers, CEO's"
+        content="A friendly reminder to all Developers, Designers, CEOs"
       />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content="Should I Deploy Today" />
       <meta
         name="twitter:description"
-        content="A friendly reminder to all Developers, Designers, CEO's"
+        content="A friendly reminder to all Developers, Designers, CEOs"
       />
 
       <meta property="og:title" content="Should I Deploy Today" />
       <meta
         property="og:description"
-        content="A friendly reminder to all Developers, Designers, CEO's"
+        content="A friendly reminder to all Developers, Designers, CEOs"
       />
       <meta property="og:url" content="https://shouldideploy.today" />
       <meta property="og:site_name" content="Should I Deploy Today" />
